f13dc43509929c3538072ce6e6fb336bcdb692bb
Lucas Felix <lucariozin.net@gmail.com>
2023-01-06 11:17:30 -0300

    correcting the main.tsx file.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/theme'
+
+import { GlobalStyles } from './styles/global'
+
 import { CycleContextProvider } from '@contexts/CycleContext'
 import { CycleFormContextProvider } from '@contexts/CycleFormContext'
 
 import { App } from './App'
-
-import { GlobalStyles } from './styles/global'
-import { theme } from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
